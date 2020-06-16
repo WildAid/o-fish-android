b6a375386f83d58ae92d8e053c8073a1eabff54e
Vitalii <vitalii_hrechenko@epam.com>
2020-06-16 11:11:26 +0300

    #20 Prompt "are you still on duty?" 10 hours after going on duty

---
+++ app/src/main/java/org/wildaid/ofish/ui/home/TimerReceiver.kt
@@ -0,0 +1,26 @@
+package org.wildaid.ofish.ui.home
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import org.wildaid.ofish.R
+import org.wildaid.ofish.app.CHANNEL_ID
+
+private const val notificationId = 119
+
+class TimerReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        val builder = NotificationCompat.Builder(context, CHANNEL_ID)
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setContentTitle(context.getString(R.string.app_name))
+            .setContentText("Are you still on duty?")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+        with(NotificationManagerCompat.from(context)) {
+            notify(notificationId, builder.build())
+        }
+    }
+}
