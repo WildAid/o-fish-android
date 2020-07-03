be38038103f7292ecd10debc83a2f1552065d1bd
VitaliyGre <vitalii_hrechenko@epam.com>
2020-07-03 15:26:15 +0300

    #125 Latitude and Longitude precision (#132)

--- app/src/main/java/org/wildaid/ofish/util/LocationUtils.kt
+++ app/src/main/java/org/wildaid/ofish/util/LocationUtils.kt
@@ -11,15 +11,28 @@ fun convert(coordinate: Double?, type: Int): String {
 }
 
 private fun format(coordinate: Double, type: Int): String {
-    val snwe = when {
+    val snwe = extractDirection(type, coordinate)
+    val strRepresentation = Location
+        .convert(coordinate, Location.FORMAT_SECONDS)
+        .substringAfter('-')
+        .substringBefore('.')
+
+    val coordinateList = strRepresentation
+        .split(":")
+        .map {
+            if (it.length == 1) "0$it" else it
+        }
+    if (coordinateList.size != 3) return ""
+
+    return "$snwe${coordinateList[0]}°${coordinateList[1]}'${coordinateList[2]}\""
+}
+
+private fun extractDirection(type: Int, coordinate: Double): String {
+    return when {
         type == LATITUDE && coordinate < 0 -> "S"
         type == LATITUDE && coordinate >= 0 -> "N"
         type == LONGITUDE && coordinate < 0 -> "W"
         type == LONGITUDE && coordinate >= 0 -> "E"
         else -> ""
     }
-    val str = Location.convert(coordinate, Location.FORMAT_SECONDS)
-    val list = str.split(":").toMutableList()
-    if (list[0].startsWith("-")) list[0] = list[0].substring(1)
-    return "$snwe${list[0]}°${list[1]}'${list[2]}\""
 }
