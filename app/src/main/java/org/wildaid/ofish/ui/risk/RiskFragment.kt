81f01bd5d0c297a364b46cdde5c9c5378c51f430
Vitalii Lebedynskyi <58213750+lebedynskyi@users.noreply.github.com>
2020-08-19 18:59:03 +0300

    Redesigned Risk view model. Now reason always visible (#230)

--- app/src/main/java/org/wildaid/ofish/ui/risk/RiskFragment.kt
+++ app/src/main/java/org/wildaid/ofish/ui/risk/RiskFragment.kt
@@ -7,6 +7,8 @@ import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import org.wildaid.ofish.EventObserver
 import org.wildaid.ofish.R
+import org.wildaid.ofish.data.SafetyColor
+import org.wildaid.ofish.data.report.SafetyLevel
 import org.wildaid.ofish.databinding.FragmentRiskBinding
 import org.wildaid.ofish.ui.base.BaseReportFragment
 import org.wildaid.ofish.util.getViewModelFactory
@@ -15,7 +17,7 @@ import org.wildaid.ofish.util.hideKeyboard
 class RiskFragment : BaseReportFragment(R.layout.fragment_risk) {
     private lateinit var viewDataBinding: FragmentRiskBinding
     private val fragmentViewModel: RiskViewModel by viewModels { getViewModelFactory() }
-    private lateinit var buttonList: List<Button>
+    private lateinit var buttonList: Map<String, View>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,35 +25,34 @@ class RiskFragment : BaseReportFragment(R.layout.fragment_risk) {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        viewDataBinding = FragmentRiskBinding.bind(view)
-            .apply {
-                this.viewmodel = fragmentViewModel
-                this.lifecycleOwner = this@RiskFragment.viewLifecycleOwner
-                buttonList = listOf(this.btnGreen, this.btnAmber, this.btnRed)
-            }
-
-        fragmentViewModel.selectedButtonLiveData.observe(
-            viewLifecycleOwner,
-            Observer { selectButton(it) })
-
-        fragmentViewModel.buttonId.observe(
-            viewLifecycleOwner, EventObserver(::onButtonClicked)
-        )
-    }
-
-    private fun selectButton(id: Int) {
-        buttonList.forEach {
-            it.isSelected = it.id == id
+        viewDataBinding = FragmentRiskBinding.bind(view).apply {
+            this.viewmodel = fragmentViewModel
+            this.lifecycleOwner = this@RiskFragment.viewLifecycleOwner
+            buttonList = mapOf(
+                SafetyColor.Green.name to btnGreen,
+                SafetyColor.Amber.name to btnAmber,
+                SafetyColor.Red.name to btnRed
+            )
         }
-        viewDataBinding.noteEditLayout.hint = getString(R.string.reason_for_risk, fragmentViewModel.getRiskColor())
+
+        fragmentViewModel.riskLiveData.observe(viewLifecycleOwner, Observer {
+            updateRisks(it)
+        })
+
+        fragmentViewModel.userEventsLiveData.observe(viewLifecycleOwner, EventObserver() {
+            proceedNext()
+        })
     }
 
-    private fun onButtonClicked(buttonId: Int) {
+    private fun updateRisks(risk: SafetyLevel) {
+        buttonList.forEach { (k, v) ->
+            v.isSelected = k == risk.level
+        }
+        viewDataBinding.noteEditLayout.hint = getString(R.string.reason_for_risk, risk.level)
+    }
+
+    private fun proceedNext() {
         hideKeyboard()
-        when (buttonId) {
-            R.id.btn_next -> {
-                onNextListener.onNextClicked()
-            }
-        }
+        onNextListener.onNextClicked()
     }
 }
--- app/src/main/java/org/wildaid/ofish/ui/risk/RiskViewModel.kt
+++ app/src/main/java/org/wildaid/ofish/ui/risk/RiskViewModel.kt
@@ -1,50 +1,48 @@
 package org.wildaid.ofish.ui.risk
 
 import android.app.Application
-import android.view.View
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import org.wildaid.ofish.Event
-import org.wildaid.ofish.R
 import org.wildaid.ofish.data.SafetyColor
 import org.wildaid.ofish.data.report.Report
+import org.wildaid.ofish.data.report.SafetyLevel
 
 class RiskViewModel(application: Application) : AndroidViewModel(application) {
+    val riskLiveData = MutableLiveData<SafetyLevel>()
+    val userEventsLiveData = MutableLiveData<Event<RiskUserEvent>>()
 
-    val noteVisibility = MutableLiveData(View.GONE)
-    val selectedButtonLiveData = MutableLiveData<Int>()
-    val buttonId = MutableLiveData<Event<Int>>()
     lateinit var report: Report
-    var note = ""
+    lateinit var safetyLevel: SafetyLevel
 
     fun initReport(currentReport: Report) {
         report = currentReport
-        report.inspection?.summary?.safetyLevel?.level = SafetyColor.Green.name
-        selectedButtonLiveData.value = R.id.btn_green
-    }
-
-    fun onActionClicked() {
-        report.inspection?.summary?.safetyLevel?.level?.let {
-            if (it.isNotBlank()) {
-                noteVisibility.value = View.VISIBLE
-            }
+        report.inspection?.summary?.safetyLevel?.let {
+            safetyLevel = it.apply { level = SafetyColor.Green.name }
+            riskLiveData.value = safetyLevel
         }
     }
 
-    fun onButtonClicked(id: Int) {
-        report.inspection?.summary?.safetyLevel?.level = when (id) {
-            R.id.btn_green -> SafetyColor.Green.name
-            R.id.btn_amber -> SafetyColor.Amber.name
-            R.id.btn_red -> SafetyColor.Red.name
-            else -> ""
-        }
+    fun onGreenChosen() {
+        safetyLevel.level = SafetyColor.Green.name
+        riskLiveData.value = safetyLevel
+    }
 
-        selectedButtonLiveData.value = id
+    fun onAmberChosen() {
+        safetyLevel.level = SafetyColor.Amber.name
+        riskLiveData.value = safetyLevel
+    }
+
+    fun onRedChosen() {
+        safetyLevel.level = SafetyColor.Red.name
+        riskLiveData.value = safetyLevel
     }
 
     fun onNextClicked() {
-        buttonId.value = Event(R.id.btn_next)
+        userEventsLiveData.value = Event(RiskUserEvent.NextEvent)
     }
 
-    fun getRiskColor() = report.inspection?.summary?.safetyLevel?.level
+    sealed class RiskUserEvent {
+        object NextEvent : RiskUserEvent()
+    }
 }
