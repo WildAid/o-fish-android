d7fff36c6e3e838f512f2174328464e195721f9b
Vitalii Lebedynskyi <vetal.lebed@gmail.com>
2020-08-27 14:06:34 +0300

    Added tests for BaseSearchViewModel

---
+++ app/src/test/java/org/wildaid/ofish/ui/search/complex/ComplexSearchViewModelTest.kt
@@ -0,0 +1,12 @@
+package org.wildaid.ofish.ui.search.complex
+
+import org.junit.Before
+
+import org.junit.Assert.*
+
+class ComplexSearchViewModelTest {
+
+    @Before
+    fun setUp() {
+    }
+}
