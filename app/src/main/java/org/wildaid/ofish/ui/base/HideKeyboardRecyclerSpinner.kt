1b4f30bf9d4a47d1591f32f8b44a794c17262636
Vitalii Lebedynskyi <vetal.lebed@gmail.com>
2020-10-01 14:06:28 +0300

    Added hiding if keyaboard if user press spinner

--- app/src/main/java/org/wildaid/ofish/ui/base/HideKeyboardRecyclerSpinner.kt
+++ app/src/main/java/org/wildaid/ofish/ui/base/HideKeyboardRecyclerSpinner.kt
@@ -3,13 +3,11 @@ package org.wildaid.ofish.ui.base
 import android.content.Context
 import android.util.AttributeSet
 import android.view.MotionEvent
-import androidx.core.widget.NestedScrollView
-import androidx.recyclerview.widget.RecyclerView
 import org.wildaid.ofish.util.hideKeyboard
 
-class HideKeyboardRecyclerView @JvmOverloads constructor(
+class HideKeyboardRecyclerSpinner @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
-) : RecyclerView(context, attrs, defStyleAttr) {
+) : androidx.appcompat.widget.AppCompatSpinner(context, attrs, defStyleAttr) {
 
     override fun onTouchEvent(ev: MotionEvent?): Boolean {
         hideKeyboard()
