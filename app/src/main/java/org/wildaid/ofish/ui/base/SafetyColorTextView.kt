e6d09cf35a6e3101381eaa79c77a54dc73f4ee04
Andre Ippisch <andre.ippisch@metronom.com>
2020-10-12 17:29:51 +0200

    Support dark mode in pill layout programmatically

--- app/src/main/java/org/wildaid/ofish/ui/base/SafetyColorTextView.kt
+++ app/src/main/java/org/wildaid/ofish/ui/base/SafetyColorTextView.kt
@@ -1,12 +1,14 @@
 package org.wildaid.ofish.ui.base
 
 import android.content.Context
+import android.graphics.Typeface
 import android.graphics.drawable.GradientDrawable
 import android.util.AttributeSet
 import androidx.annotation.ColorRes
 import androidx.annotation.DimenRes
 import org.wildaid.ofish.R
 import org.wildaid.ofish.data.SafetyColor
+import org.wildaid.ofish.data.report.DarkMode
 
 class SafetyColorTextView @JvmOverloads constructor(
     context: Context,
@@ -14,7 +16,15 @@ class SafetyColorTextView @JvmOverloads constructor(
     defStyle: Int = 0
 ) : androidx.appcompat.widget.AppCompatTextView(context, attrs, defStyle) {
 
-    fun setSafetyColor(safetyColor: SafetyColor, @DimenRes radiusRes: Int) {
+    fun setSafetyColor(safetyColor: SafetyColor, @DimenRes radiusRes: Int, darkMode: DarkMode?) {
+        if (darkMode?.enabled == false) {
+            setSafetyColorForLightMode(safetyColor, radiusRes)
+        } else {
+            setSafetyColorForDarkMode(safetyColor, radiusRes)
+        }
+    }
+
+    private fun setSafetyColorForLightMode(safetyColor: SafetyColor, @DimenRes radiusRes: Int) {
         when (safetyColor) {
             SafetyColor.Red -> {
                 text = SafetyColor.Red.name
@@ -32,13 +42,52 @@ class SafetyColorTextView @JvmOverloads constructor(
                 background = createGradientDrawable(R.color.amber_background, radiusRes)
             }
         }
+
+        setTypeface(null, Typeface.BOLD)
     }
 
-    private fun createGradientDrawable(@ColorRes colorRes: Int, @DimenRes radiusRes: Int): GradientDrawable {
+    private fun setSafetyColorForDarkMode(safetyColor: SafetyColor, @DimenRes radiusRes: Int) {
+        when (safetyColor) {
+            SafetyColor.Red -> {
+                text = SafetyColor.Red.name
+                setTextColor(context.getColor(R.color.red_night))
+                background = createDarkModeGradientDrawable(R.color.red_night, radiusRes)
+            }
+            SafetyColor.Green -> {
+                text = SafetyColor.Green.name
+                setTextColor(context.getColor(R.color.green_night))
+                background = createDarkModeGradientDrawable(R.color.green_night, radiusRes)
+            }
+            SafetyColor.Amber -> {
+                text = SafetyColor.Amber.name
+                setTextColor(context.getColor(R.color.amber_night))
+                background = createDarkModeGradientDrawable(R.color.amber_night, radiusRes)
+            }
+        }
+
+        setTypeface(null, Typeface.NORMAL)
+    }
+
+    private fun createGradientDrawable(
+        @ColorRes colorRes: Int,
+        @DimenRes radiusRes: Int
+    ): GradientDrawable {
         val gd = GradientDrawable()
         gd.shape = GradientDrawable.RECTANGLE
         gd.cornerRadius = resources.getDimension(radiusRes)
         gd.setColor(resources.getColor(colorRes, null))
+        return gd
+    }
+
+    private fun createDarkModeGradientDrawable(
+        @ColorRes colorRes: Int,
+        @DimenRes radiusRes: Int
+    ): GradientDrawable {
+        val gd = GradientDrawable()
+        gd.shape = GradientDrawable.RECTANGLE
+        gd.setStroke(4, resources.getColor(colorRes, null))
+        gd.cornerRadius = resources.getDimension(radiusRes)
+        gd.setColor(resources.getColor(R.color.black_1, null))
         return gd
     }
 }
