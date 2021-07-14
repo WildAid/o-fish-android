4a60ea3385d31b1a9fdfac22ad9e39fc8c69ca5f
Mohit Sharma <mohit7575@gmail.com>
2021-07-14 14:36:24 +0100

    Bugfix 441 (#442)
    * upgraded gradle app version
    
    * bug fix 441
    
    * Allow on UI thread disabled

--- app/src/main/java/org/wildaid/ofish/data/RealmDataSource.kt
+++ app/src/main/java/org/wildaid/ofish/data/RealmDataSource.kt
@@ -173,9 +173,9 @@ class RealmDataSource(context: Context) {
         realm.executeTransactionAsync(
             {
                 while (photoIterator.hasNext()) {
-                    it.insert(photoIterator.next())
+                    it.insertOrUpdate(photoIterator.next())
                 }
-                it.insert(report)
+                it.insertOrUpdate(report)
             },
             { listener.onSuccess() },
             { listener.onError(it) }
@@ -331,7 +331,6 @@ class RealmDataSource(context: Context) {
 
         val configuration = SyncConfiguration
             .Builder(user, agencyName)
-            .allowWritesOnUiThread(true)
             .build()
         realm = Realm.getInstance(configuration)
     }
